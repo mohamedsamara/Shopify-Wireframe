8940a7f2059474363bccce43cdd7267ec7753795
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-04-21 02:49:20 -0700

    add promotion countdown timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,45 @@ function showSlides() {
   dots[slideIndex - 1].className += ' active';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+// timer countdown
+
+function calculateTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    total: t,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  };
+}
+
+function startTimer(id, endtime) {
+  var clock = document.getElementById(id);
+  var hoursBlock = clock.querySelector('.hours');
+  var minutesBlock = clock.querySelector('.minutes');
+  var secondsBlock = clock.querySelector('.seconds');
+
+  function updateTimer() {
+    var time = calculateTimeRemaining(endtime);
+
+    hoursBlock.innerHTML = ('0' + time.hours).slice(-2);
+    minutesBlock.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsBlock.innerHTML = ('0' + time.seconds).slice(-2);
+
+    if (time.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateTimer();
+  var timeinterval = setInterval(updateTimer, 1000);
+}
+
+var promotionEndTime = new Date(
+  Date.parse(new Date()) + 1 * 24 * 60 * 60 * 1000
+);
+startTimer('promotion-timer', promotionEndTime);
